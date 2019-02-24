4f2e08a9ab0b0aa186cc80573df0139aaa1a4057
Joshua Stephenson-Losey <35410829+JDSL1996@users.noreply.github.com>
2019-02-24 07:53:12 -0700

    add loop exit uppon accept or not accept

--- Driver2.java
+++ Driver2.java
@@ -6,16 +6,16 @@ import java.io.*;
 public class Driver2 {
     public static void main(String[] args) {
         // read method to get the source code
-        File f ;
+        File f;
         String file;
         scannerLexer lexer;
         boolean done = false;
-        while(!done){
-            try{
-                f =  new File("./inputs/"+args[0]);
+        while (!done) {
+            try {
+                f = new File("./inputs/" + args[0]);
                 file = new String(Files.readAllBytes(f.toPath()));
                 lexer = new scannerLexer(new ANTLRInputStream(file));
-                
+
                 // printer for output
                 File out = new File("out.txt");
                 FileWriter fw = new FileWriter(out);
@@ -24,41 +24,41 @@ public class Driver2 {
                 scannerParser parser = new scannerParser(cts);
 
                 parser.setErrorHandler(new BailErrorStrategy());
-                try{
+                try {
                     parser.program();
                     writer.write("Accepted");
                     writer.flush();
-                }catch(Exception e){
+                } catch (Exception e) {
                     writer.write("Not accepted");
                     writer.flush();
                     System.exit(1);
                 }
                 // always close
-                finally{
+                finally {
+                    done = true;
                     writer.close();
                 }
-            }catch (NullPointerException e){
-                try{
-                    //else try in current directory before saying it dosent exist
-                    f =  new File(args[0]);
+            } catch (NullPointerException e) {
+                try {
+                    // else try in current directory before saying it dosent exist
+                    f = new File(args[0]);
                     file = new String(Files.readAllBytes(f.toPath()));
                     lexer = new scannerLexer(new ANTLRInputStream(file));
-                }catch(NullPointerException n){
+                } catch (NullPointerException n) {
                     System.out.println("Checked current directory and ./inputs, no file found");
                     done = true;
                     n.printStackTrace();
-                }
-                catch(FileNotFoundException fnfe){
+                } catch (FileNotFoundException fnfe) {
                     done = true;
                     System.out.println("Checked current directory and ./inputs, no file found");
-                }catch(IOException ioe){
+                } catch (IOException ioe) {
                     done = true;
                     ioe.printStackTrace();
                 }
-            }catch(FileNotFoundException fnfe){
+            } catch (FileNotFoundException fnfe) {
                 done = true;
                 System.out.println("Checked current directory and ./inputs, no file found");
-            }catch(IOException ioe){
+            } catch (IOException ioe) {
                 done = true;
                 ioe.printStackTrace();
             }
