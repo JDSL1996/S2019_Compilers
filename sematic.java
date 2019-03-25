a22410393c52ec567e94fa1fa6847050a860bd6b
Joshua Stephenson-Losey <35410829+JDSL1996@users.noreply.github.com>
2019-03-25 16:52:59 -0600

    settup for step 3

---
+++ sematic.java
@@ -0,0 +1,7 @@
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+public class sematic extends scannerBaseListener {
+    @Override
+    public void enterProgram(scannerParser.ProgramContext ctx) {
+    }
+}
